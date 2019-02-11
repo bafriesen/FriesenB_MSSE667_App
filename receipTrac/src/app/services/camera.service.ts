11d8d165cd0138462471e2709044b1271f72e0b7
bafriesen <friesen.benjamin@gmail.com>
2019-02-10 19:04:58 -0700

    commit

--- receipTrac/src/app/services/camera.service.ts
+++ receipTrac/src/app/services/camera.service.ts
@@ -10,17 +10,17 @@ export class CameraService {
   takePicture() {
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
+      destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
-
+    
     this.camera.getPicture(options).then((imageData) => {
-      // Do something with the new photo
-
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
     }, (err) => {
      // Handle error
-     console.log("Camera issue: " + err);
     });
   }
 }
--- receipTrac/src/app/tab1/tab1.page.ts
+++ receipTrac/src/app/tab1/tab1.page.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { CameraService } from '../services/camera.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  
+  constructor(private camera: CameraService){}
+
+  takePhoto(camera){
+    this.camera.takePicture();
+  }
+
+  saveReceipt(){
+    console.log('Saved your receipt');
+  }
+}
